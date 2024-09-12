28eb4d7b7a0b7634515492c25af8f8109d57a486
Arthur COMET <acomet@oci.fr>
2024-09-12 15:56:27 +0200

    add: user/:email and put user/:id, possibility to edit the table with the id

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,7 +10,6 @@ const authenticateJWT = (req, res, next) => {
             if (err) {
                 return res.sendStatus(403);
             }
-
             req.user = user;
             next();
         });
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -24,23 +24,59 @@ router.get("/:id", (req, res) => {
             return res.status(500).json({ error: 'Database query failed' });
         }
         if (results.length === 0) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'Userid not found' });
         }
         res.json(results[0]);
     });
 });
 
-router.get("/:id/:todos", (req, res) => {
-    const id = req.params.id;
+router.get("/email/:email", (req, res) => {
+    const email = req.params.email;
 
-    pool.query('SELECT * FROM todo WHERE id = ?', [id], (error, results) => {
+    pool.query('SELECT * FROM user WHERE email = ?', [email], (error, results) => {
         if (error) {
             return res.status(500).json({ error: 'Database query failed' });
         }
         if (results.length === 0) {
-            return res.status(404).json({ message: 'Todo not found' });
+            return res.status(404).json({ message: 'User not found' });
         }
         res.json(results[0]);
+    });
+})
+
+router.get("/todos", (req, res) => {
+    const userId = req.user.id;
+
+    pool.query('SELECT * FROM todo WHERE user_id = ?', [userId], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'No todos found for this user' });
+        }
+        res.json(results);
+    });
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const { email, name, firstname, created_at, password } = req.body;
+
+    const isValidDate = (date) => {
+        return !isNaN(Date.parse(date));
+    };
+
+    if (!isValidDate(created_at)) {
+        return res.status(400).json({ error: 'Invalid date format for created_at' });
+    }
+
+    pool.query('UPDATE user SET email = ?, name = ?, firstname = ?, created_at = ?, password = ? WHERE id = ?',
+        [email, name, firstname, created_at, password, id], (error, results) => {
+        if (error) {
+            console.error('Database query failed:', error);
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        res.json({ message: 'User updated successfully' });
     });
 });
 
